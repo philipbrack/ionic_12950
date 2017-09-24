8763b2ac94409424ce9b4299f6941f5ff15bf24a
Philip Brack <philip.brack@gmail.com>
2017-09-24 09:43:35 -0700

    commit for issue https://github.com/ionic-team/ionic/issues/12950

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {ViewChild} from '@angular/core';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  isPaused: boolean;
+  @ViewChild('testspinner') testspinner;
 
-  constructor(public navCtrl: NavController) {
+  constructor() {
+    this.isPaused = true;
+  }
 
+  containsPausedClass(list: string[]) {
+    return (list.indexOf('spinner-paused') != -1);
+  }
+
+  ngAfterViewInit() {
+    console.log('test spinner is paused ',
+      this.containsPausedClass(this.testspinner._elementRef.nativeElement.classList.value));
+  }
+
+  togglePause() {
+    this.isPaused = !this.isPaused;
+
+    setTimeout(() => {
+
+      console.log('test spinner is paused ',
+        this.containsPausedClass(this.testspinner._elementRef.nativeElement.classList.value));
+    }, 100);
   }
 
 }
