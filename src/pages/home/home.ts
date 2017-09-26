8ea0fa9c78d0632dadbe02813dfbda9426ca1868
Philip Brack <philip.brack@gmail.com>
2017-09-25 20:03:46 -0700

    Workaround for issue.  If I allow the element to load without the 'spinner-paused' css attribute then apply it afterward this works as expected on safari browser.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,16 +11,22 @@ export class HomePage {
   @ViewChild('testspinner') testspinner;
 
   constructor() {
-    this.isPaused = true;
+    this.isPaused = false; // set to true will cause to never have animation state running.
   }
+
 
   containsPausedClass(list: string[]) {
     return (list.indexOf('spinner-paused') != -1);
   }
 
   ngAfterViewInit() {
+    // if the spinner is allows to load without 'spinner-paused' then safari will work.
+    setTimeout(() => {
+      this.isPaused = true;
+    }, 0);
     console.log('test spinner is paused ',
       this.containsPausedClass(this.testspinner._elementRef.nativeElement.classList.value));
+
   }
 
   togglePause() {
